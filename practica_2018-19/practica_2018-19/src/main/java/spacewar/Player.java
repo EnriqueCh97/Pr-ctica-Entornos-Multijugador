c6c87af23d82f4cea008634acca9a3b61d3c01ce
Alberto-HG <43338527+Alberto-HG@users.noreply.github.com>
2019-05-25 17:53:28 +0200

    Add files via upload

---
+++ practica_2018-19/practica_2018-19/src/main/java/spacewar/Player.java
@@ -0,0 +1,52 @@
+package spacewar;
+
+import java.util.Random;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+public class Player extends Spaceship {
+
+	private final WebSocketSession session;
+	private final int playerId;
+	private final String shipType;
+	private String name;
+
+	public Player(int playerId, WebSocketSession session) {
+		this.playerId = playerId;
+		this.session = session;
+		this.shipType = this.getRandomShipType();
+	}
+
+	public int getPlayerId() {
+		return this.playerId;
+	}
+	
+	public String getPlayerName() {
+		return this.name;
+	}
+
+	public void setPlayerName(String name) {
+		this.name = name;
+	}
+
+
+	public WebSocketSession getSession() {
+		return this.session;
+	}
+
+	public void sendMessage(String msg) throws Exception {
+		this.session.sendMessage(new TextMessage(msg));
+	}
+
+	public String getShipType() {
+		return shipType;
+	}
+
+	private String getRandomShipType() {
+		String[] randomShips = { "blue", "darkgrey", "green", "metalic", "orange", "purple", "red" };
+		String ship = (randomShips[new Random().nextInt(randomShips.length)]);
+		ship += "_0" + (new Random().nextInt(5) + 1) + ".png";
+		return ship;
+	}
+}
